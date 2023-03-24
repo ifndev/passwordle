16c313d6344be85a88e9d985ec4253ea8dddb3b7
Patrick Conti <contact@patrickconti.fr>
2023-03-24 11:58:08 +0100

    working game

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,111 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import passwords from "./helpers/passwords";
+import Word from "./components/Word";
+import Tries from "./components/Tries";
+import Keyboard from "./components/Keyboard";
+import useKeypress from "./hooks/useKeypress";
 import "./App.css";
 
-const title = "app";
-
 function App() {
-  const [count, setCount] = useState(0);
+  const [target, setTarget] = useState<string>("test");
+  const [input, setInput] = useState<string>("");
+  const [tries, setTries] = useState<string[]>([]);
+  const [validLetters, setValidLetters] = useState<number[]>([]);
+
+  // Initialize alphabet
+  let validKeys: Array<string> = [];
+  // Add all letters a to z to the alphabet, both upper and lower case
+  for (let i = 97; i < 123; i++) {
+    validKeys.push(String.fromCharCode(i));
+    validKeys.push(String.fromCharCode(i).toUpperCase());
+  }
+  // Add all numbers 0 to 9 to the alphabet
+  for (let i = 48; i < 58; i++) {
+    validKeys.push(String.fromCharCode(i));
+  }
+
+  // Iterate over all passwords and add all letters to the alphabet to ensure that all letters and special characters are included
+  passwords.forEach((password) => {
+    password.split("").forEach((letter) => {
+      if (!validKeys.includes(letter)) {
+        validKeys.push(letter);
+      }
+    });
+  });
+
+  useEffect(() => {
+    // Choose a target at random
+    const newTarget = passwords[Math.floor(Math.random() * passwords.length)];
+    setTarget(newTarget);
+
+    console.log("Target is:" + newTarget);
+  }, [setTarget]);
+
+  /**
+   * Check if the key is valid
+   * @param key key to check
+   * @returns boolean
+   */
+  const keyValid = (key: string) => {
+    return validKeys.includes(key);
+  };
+
+  const onKeyPressed = (key: string) => {
+    if (key === "Backspace") {
+      popLetter();
+    } else if (key === "Enter") {
+      submitTry();
+    } else if (keyValid(key)) {
+      pushLetter(key);
+    }
+  };
+
+  const pushLetter = (letter: string) => {
+    if (input.length === target.length) return;
+    setInput(input + letter);
+  };
+
+  const popLetter = () => {
+    if (input.length === 0) return;
+    setInput(input.slice(0, -1));
+  };
+
+  const tryValid = () => {
+    return input.length === target.length;
+  };
+
+  const submitTry = () => {
+    if (tryValid()) {
+      setTries([...tries, input]);
+      setInput("");
+
+      let validLetters: Array<number> = [];
+      input.split("").forEach((letter, index) => {
+        if (letter.toLowerCase() === target[index].toLowerCase()) {
+          validLetters.push(index);
+        }
+      });
+      setValidLetters(validLetters);
+    }
+  };
+
+  // Hooks
+  useKeypress(onKeyPressed);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App flex flex-col gap-10">
+      <h1>Passwordle</h1>
+      <div className="board flex flex-col gap-2">
+        <Tries target={target} tries={tries} />
+        <Word
+          targetWord={target}
+          input={input}
+          colorize={false}
+          validLetters={validLetters}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Keyboard targetWord={target} tries={tries} alphabet={validKeys} />
     </div>
   );
 }
-
 export default App;
