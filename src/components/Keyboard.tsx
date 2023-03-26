6a9917ec4d0e97abfafd78e8120bcdcc178a5757
Patrick Conti <contact@patrickconti.fr>
2023-03-26 12:36:18 +0200

    Layout de clavier Fixes #4

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,18 +1,40 @@
-import { Guess } from "../helpers/gameUtils";
+import { Guess, CharType, checkType } from "../helpers/gameUtils";
+import { useState } from "react";
 
 function Keyboard({
   targetWord,
   guesses,
   alphabet,
+  callback,
 }: {
   targetWord: string;
   guesses: Guess[];
   alphabet: string[];
+  callback: Function;
 }) {
-  alphabet = alphabet.filter((key) => {
-    // Remove uppercase letters
-    return !key.match(/^[A-Z]$/);
+  const [upperCase, setUppercase] = useState(false);
+
+  const layoutLower = [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l", "Enter"],
+    ["Caps", "z", "x", "c", "v", "b", "n", "m", "Backspace"],
+  ];
+
+  const layoutUpper = [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter"],
+    ["Caps", "Z", "X", "C", "V", "B", "N", "M", "Backspace"],
+  ];
+
+  const layout = upperCase ? layoutUpper : layoutLower;
+
+  const symbols = alphabet.filter((key) => {
+    // Keep only symbols
+    return checkType(key) === CharType.SYMBOL;
   });
+  layout.push(symbols);
 
   const guessedWords = guesses.map((guess) => {
     return guess.guessedWord;
@@ -36,22 +58,51 @@ function Keyboard({
     });
   });
 
-  const keys = alphabet.map((key, index) => {
-    let style = "";
-    if (correctlyPlacedLetters.includes(key)) style = "bg-green-700";
-    else if (guessedLetters.includes(key)) style = "bg-yellow-700";
-    else if (triedLetters.includes(key))
-      style = "border-gray-700 text-gray-700";
+  /// Get a line of keys
+  const getKeyboardLine = (characters: Array<string>) => {
+    return characters.map((key, index) => {
+      let style = "";
+      if (correctlyPlacedLetters.includes(key.toLowerCase()))
+        style = "bg-green-700";
+      else if (guessedLetters.includes(key.toLowerCase()))
+        style = "bg-yellow-700";
+      else if (triedLetters.includes(key.toLowerCase()))
+        style = "border-gray-700 text-gray-700";
 
+      return (
+        <div
+          key={index}
+          className={`border-2 border-gray-400 p-2 ${style} select-none w-8`}
+          data-testid={`keyboard-key-${key}`}
+          onClick={
+            key === "Caps"
+              ? () => setUppercase(!upperCase)
+              : () => callback(key)
+          }
+        >
+          {getDisplayKey(key)}
+        </div>
+      );
+    });
+  };
+
+  const getDisplayKey = (key: string) => {
+    if (key === "Enter") return "↵";
+    if (key === "Backspace") return "⌫";
+    if (key === "Caps") return upperCase ? "⇧" : "⇪";
+    return key;
+  };
+
+  const keys = layout.map((line, index) => {
     return (
-      <div key={index} className={`border-2 border-gray-400 p-2 ${style}`}>
-        {key}
+      <div className="flex flex-row justify-center gap-2">
+        {getKeyboardLine(line)}
       </div>
     );
   });
 
   return (
-    <div className="grid grid-cols-10 gap-2" data-testid="keyboard">
+    <div className="flex flex-col justify-center gap-2" data-testid="keyboard">
       {keys}
     </div>
   );
