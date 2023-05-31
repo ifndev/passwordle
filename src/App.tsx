7a018aafb4db2c4a857a7586651f85c98a152ecc
Patrick Conti <contact@patrickconti.fr>
2023-05-31 15:42:49 +0200

    Animate guess validity reveal on submit Fixes #11

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import useKeypress from "./hooks/useKeypress";
 import "./App.css";
 import useGame from "./hooks/useGame";
 import { Guess, LetterValidity, Status } from "./helpers/gameUtils";
+import { ANIMTIME } from "./constants/settings";
 
 function App({ target, maxGuesses }: { target?: string; maxGuesses?: number }) {
   const [input, setInput] = useState<string>("");
@@ -13,6 +14,7 @@ function App({ target, maxGuesses }: { target?: string; maxGuesses?: number }) {
     target: target,
     tries: maxGuesses,
   });
+  const [isRevealing, setIsRevealing] = useState<boolean>(false);
 
   /**
    * Check if the key is valid
@@ -54,7 +56,17 @@ function App({ target, maxGuesses }: { target?: string; maxGuesses?: number }) {
     if (guessValid()) {
       dispatch({ type: "guess", payload: { guess: input } });
       setInput("");
+      reveal();
     }
+  };
+
+  const reveal = () => {
+    setIsRevealing(true);
+
+    // Wait until the animation is done
+    setTimeout(() => {
+      setIsRevealing(false);
+    }, ANIMTIME * (game.targetWord.length + 1));
   };
 
   // TODO: Move this to the game utils eventually
@@ -94,7 +106,11 @@ function App({ target, maxGuesses }: { target?: string; maxGuesses?: number }) {
     <div className="App flex flex-col gap-10">
       <h1>Passwordle</h1>
       <div className="board flex flex-col gap-2" data-testid="board">
-        <Guesses target={game.targetWord} guesses={game.guesses} />
+        <Guesses
+          target={game.targetWord}
+          guesses={game.guesses}
+          isRevealing={isRevealing}
+        />
         {word()}
       </div>
       <Keyboard
---
+++ src/constants/settings.ts
@@ -0,0 +1,1 @@
+export const ANIMTIME = 200;
